50d95d26690eb582a39ca4022384205179e1f5c5
Aquatic <aquatic@skiff.com>
2023-03-19 23:30:30 -0600

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link"
 import * as React from "react"
 
-import { Seo } from "@/components"
+import { Layout, Seo } from "@/components"
 
 export default function NotFoundPage() {
   return (
-    <>
+    <Layout>
       <Seo templateTitle="Not Found" />
 
-      <main>404</main>
-    </>
+      <div className="layout">
+        <main className="py-8 text-center">
+          <h1 className="mb-3 text-xl font-medium">404 - Not Found</h1>
+          <p>
+            There's nothing here. Try{" "}
+            <Link className="underline" href="/">
+              going home
+            </Link>
+            .
+          </p>
+        </main>
+      </div>
+    </Layout>
   )
 }
