b1eeca7be4e484e59187de7024b86ac04f5770a5
Aquatic <aquatic@skiff.com>
2023-03-21 18:28:42 -0600

    added margin

--- src/components/AccountOverview.tsx
+++ src/components/AccountOverview.tsx
@@ -56,7 +56,7 @@ export const AccountOverview: FC = () => {
           </dd>
         </dl>
         {isClientReady && accountMetrics.isSuccess && !accountMetrics.data?.isCollateralized && (
-          <p className="text-xs text-red">
+          <p className="mb-1 text-xs text-red">
             Auto-renewals will not be processed if your collateralization ratio is below{" "}
             {formatUnits(accountMetrics.data?.minCollatRatio ?? "2")}
           </p>
