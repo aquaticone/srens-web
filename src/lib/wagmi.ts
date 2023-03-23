bcd967556673069007179af850649df8e518beec
Aquatic <aquatic@skiff.com>
2023-03-23 17:47:33 -0600

    added llamanodes rpc as fallback

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -2,13 +2,16 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit"
 import { configureChains, createClient } from "wagmi"
 import { foundry, mainnet } from "wagmi/chains"
 import { alchemyProvider } from "wagmi/providers/alchemy"
-import { publicProvider } from "wagmi/providers/public"
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc"
 
 const isFoundryEnabled = process.env.NEXT_PUBLIC_ENABLE_FOUNDRY === "true"
 
 const { chains, provider, webSocketProvider } = configureChains(
   [mainnet, ...(isFoundryEnabled ? [foundry] : [])],
-  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? "" }), publicProvider()]
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? "" }),
+    jsonRpcProvider({ rpc: (_chain) => ({ http: "https://eth.llamarpc.com" }) }),
+  ]
 )
 
 const { connectors } = getDefaultWallets({
