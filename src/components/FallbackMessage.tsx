ce175b39c7c49e414133ab9b9f2f5e9c1d733b2c
Aquatic <aquatic@skiff.com>
2023-03-23 17:47:01 -0600

    improved mobile styling fallback message

--- src/components/FallbackMessage.tsx
+++ src/components/FallbackMessage.tsx
@@ -11,7 +11,7 @@ export const FallbackMessage: FC<PropsWithChildren<FallbackMessageProps>> = ({ c
   return (
     <div
       className={clsxm(
-        "flex h-[134px] w-full items-center justify-center rounded px-6 py-4",
+        "flex w-full items-center justify-center rounded p-4 max-md:text-sm md:h-[134px] md:px-6 md:py-4",
         { "bg-comet-800 text-comet-300": variant === "black" },
         className
       )}
